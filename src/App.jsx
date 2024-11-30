17b595153cd24ac93cf55104aa6afa7ca37f23c3
bellgushbell <gushbellpiriyapong@gmail.com>
2024-11-30 20:32:22 +0700

    add change language first2

--- src/App.jsx
+++ src/App.jsx
@@ -124,7 +124,7 @@ const App = () => {
         <Navbar />
         {/* ส่วนของปุ่มToggleแปลภาษา */}
 
-        <div className="flex justify-end items-center ml-auto p-5 mt-5">
+        <div className="absolute flex justify-end items-center p-5 right-10 top-10 ">
           <span className="mr-2">{i18n.language === "th" ? "TH" : "EN"}</span>
           <Switch
             onChange={toggleLanguage}
